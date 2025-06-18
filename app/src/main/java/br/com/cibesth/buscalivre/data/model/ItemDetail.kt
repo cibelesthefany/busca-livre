96b975e976a179e34d0cd5a42022ffdcaca90368
cibelesthefany <cibelesthefanysilva@gmail.com>
2025-06-18 13:27:55 -0300

    feat: apaga campos desnecessários

--- app/src/main/java/br/com/cibesth/buscalivre/data/model/ItemDetail.kt
+++ app/src/main/java/br/com/cibesth/buscalivre/data/model/ItemDetail.kt
@@ -1,11 +1,8 @@
 package br.com.cibesth.buscalivre.data.model
 
 data class ItemDetail(
-    val id: String,
     val title: String,
     val price: Double,
-    val condition: String,
     val thumbnail: String,
-    val available_quantity: Int,
     val pictures: List<Picture>?
 )
